cad984d63fb576d05b26d5636d1a0090028db499
classadoo-test-1 <runnedrun+classadoo1@gmail.com>
2016-10-17 10:03:45 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,17 @@
+var person = {
+  name: "David",
+  representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
+  height: "500px",	
+  favoriteColor: "blue",
+  catchphrase: "Get pumped!"
+}
+console.log("asdfasDF")
+var u = 9
+console.log("sdf")
+console.log("asdfa")
+
+drawPerson(person)
+
+function drawPerson(person) {
+  person.name = "Ralph"
+}
